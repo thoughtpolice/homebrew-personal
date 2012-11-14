a1f0ffff860724521cb2bec8072456a42cad3e55
Austin Seipp <mad.one@gmail.com>
2012-11-13 21:38:29 -0600

    Tengine 1.3.0/1.4.1
    Signed-off-by: Austin Seipp <mad.one@gmail.com>

---
+++ tengine.rb
@@ -0,0 +1,128 @@
+require 'formula'
+
+class Tengine < Formula
+  homepage 'http://nginx.org/'
+  url 'http://tengine.taobao.org/download/tengine-1.3.0.tar.gz'
+  sha1 '9f9509f6cd397a0834d93588f231af2b0ce55154'
+  head 'https://github.com/taobao/tengine.git', :using => :git
+
+  devel do
+    url 'http://tengine.taobao.org/download/tengine-1.4.1.tar.gz'
+    sha1 'ed7701fc304769d3da8df783e6886940d1ed8668'
+  end
+
+  env :userpaths
+
+  depends_on 'pcre'
+  depends_on 'luajit'
+  depends_on 'jemalloc'
+
+  option 'with-passenger', 'Compile with support for Phusion Passenger module'
+  option 'with-webdav', 'Compile with support for WebDAV module'
+  skip_clean 'logs'
+
+  # Changes default port to 8080
+  def patches
+    DATA
+  end
+
+  def passenger_config_args
+    passenger_root = `passenger-config --root`.chomp
+
+    if File.directory?(passenger_root)
+      return "--add-module=#{passenger_root}/ext/nginx"
+    end
+
+    puts "Unable to install nginx with passenger support. The passenger"
+    puts "gem must be installed and passenger-config must be in your path"
+    puts "in order to continue."
+    exit
+  end
+
+  def install
+    luajit = Formula.factory('luajit')
+    args = ["--prefix=#{prefix}",
+            "--with-http_ssl_module",
+            "--with-pcre",
+            "--with-ipv6",
+            "--with-cc-opt=-I#{HOMEBREW_PREFIX}/include",
+            "--with-ld-opt=-L#{HOMEBREW_PREFIX}/lib",
+            "--conf-path=#{etc}/nginx/nginx.conf",
+            "--pid-path=#{var}/run/nginx.pid",
+            "--lock-path=#{var}/run/nginx.lock",
+            "--with-jemalloc",
+            "--with-http_lua_module",
+            "--with-luajit-inc=#{luajit.include}/luajit-2.0", # TODO FIXME: ugly :(
+            "--with-luajit-lib=#{luajit.lib}",
+            "--http-client-body-temp-path=#{var}/run/nginx/client_body_temp",
+            "--http-proxy-temp-path=#{var}/run/nginx/proxy_temp",
+            "--http-fastcgi-temp-path=#{var}/run/nginx/fastcgi_temp",
+            "--http-uwsgi-temp-path=#{var}/run/nginx/uwsgi_temp",
+            "--http-scgi-temp-path=#{var}/run/nginx/scgi_temp"]
+
+    args << passenger_config_args if build.include? 'with-passenger'
+    args << "--with-http_dav_module" if build.include? 'with-webdav'
+
+    system "./configure", *args
+    system "make"
+    system "make install"
+    man8.install "objs/nginx.8"
+    (var/'run/nginx').mkpath
+  end
+
+  def caveats; <<-EOS.undent
+    In the interest of allowing you to run `nginx` without `sudo`, the default
+    port is set to localhost:8080.
+
+    If you want to host pages on your local machine to the public, you should
+    change that to localhost:80, and run `sudo nginx`. You'll need to turn off
+    any other web servers running port 80, of course.
+
+    You can start nginx automatically on login running as your user with:
+      mkdir -p ~/Library/LaunchAgents
+      cp #{plist_path} ~/Library/LaunchAgents/
+      launchctl load -w ~/Library/LaunchAgents/#{plist_path.basename}
+
+    Though note that if running as your user, the launch agent will fail if you
+    try to use a port below 1024 (such as http's default of 80.)
+    EOS
+  end
+
+  def startup_plist
+    return <<-EOPLIST
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+  <dict>
+    <key>Label</key>
+    <string>#{plist_name}</string>
+    <key>RunAtLoad</key>
+    <true/>
+    <key>KeepAlive</key>
+    <false/>
+    <key>UserName</key>
+    <string>#{`whoami`.chomp}</string>
+    <key>ProgramArguments</key>
+    <array>
+        <string>#{HOMEBREW_PREFIX}/sbin/nginx</string>
+    </array>
+    <key>WorkingDirectory</key>
+    <string>#{HOMEBREW_PREFIX}</string>
+  </dict>
+</plist>
+    EOPLIST
+  end
+end
+
+__END__
+--- a/conf/nginx.conf
++++ b/conf/nginx.conf
+@@ -33,7 +33,7 @@
+     #gzip  on;
+
+     server {
+-        listen       80;
++        listen       8080;
+         server_name  localhost;
+
+         #charset koi8-r;
