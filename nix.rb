395a551fb5f770cb26bc5d5a0806ff63f3414651
Austin Seipp <mad.one@gmail.com>
2012-12-17 05:36:59 -0600

    Add plist for nix-daemon, fix test method, add 'nix-init.sh' script
    Signed-off-by: Austin Seipp <mad.one@gmail.com>

--- nix.rb
+++ nix.rb
@@ -13,9 +13,55 @@ class Nix < Formula
     system "./configure --prefix=#{prefix}"
     system "make"
     system "make install"
+
+    (bin+'nix-init.sh').write <<-EOS.undent
+      #!/bin/bash
+      source #{prefix}/etc/profile.d/nix.sh
+    EOS
+    chmod 0775, bin+'nix-init.sh'
+  end
+
+  def caveats; <<-EOS.undent
+    You can use the 'nix-init.sh' script to prepare your shell environment.
+
+    You need to add the nixpkgs channel:
+
+      $ nix-channel --add http://nixos.org/channels/nixpkgs-unstable
+      $ nix-channel --update
+
+    EOS
+  end
+
+  plist_options :manual => "nix-daemon"
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>KeepAlive</key>
+      <true/>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_prefix}/bin/nix-daemon</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>UserName</key>
+      <string>#{`whoami`.chomp}</string>
+      <key>WorkingDirectory</key>
+      <string>#{var}</string>
+      <key>StandardErrorPath</key>
+      <string>#{var}/nix/daemon.log</string>
+      <key>StandardOutPath</key>
+      <string>#{var}/nix/daemon.log</string>
+    </dict>
+    </plist>
+    EOS
   end
 
   def test
-    system "true"
+    system "nix-env --help"
   end
 end
